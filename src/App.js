81c7e85ab7b687f1efb6a54cf06a5b3b1fdc135b
Stine <stinehso@gmail.com>
2020-02-14 14:55:13 +0100

    Create class and state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const state = {
-    robots: robots,
-    searchfield: ''
-}
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <Cardlist robots={robots} />
-        </div>
-    )
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    render() {
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox />
+                <Cardlist robots={robots} />
+            </div>
+        )
+    }
 }
 
 export default App;
